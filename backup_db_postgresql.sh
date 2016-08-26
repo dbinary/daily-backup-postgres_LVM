f05048ec91947b79d9c43ff8d6ed2c69c625c0f3
Luis Perez <luis.luimarin@gmail.com>
2016-08-26 16:05:38 -0600

    change logic for backup

--- backup_db_postgresql.sh
+++ backup_db_postgresql.sh
@@ -1,36 +1,25 @@
 #!/bin/bash
 
-USRSMB='user'
-PASSSMB='password'
-DOMAINSMB='Domain'
-REMOTESMB='192.168.13.15/backups/DIARIOS'
-MNTSMB='/backups'
-
 STARTUPSCRIPT="/etc/init.d/postgresql"
 LVM="/dev/Data/pgsql"
-NLVMS="pgsql-snap"
-LVMS="/dev/Data/${NLVMS}"
+NEW_LVM="pgsql-snap"
+LVMS="/dev/Data/${NEW_LVM}"
 MNTP="/mnt/pgsql"
 BZ2FILE="pgsql-backup"
-BACKUPDIR="/backup"
+BACKUPDIR="/ITM/backups"
 
-function deletefile {
-    /usr/bin/logger -i -t $(basename $0) 'Eliminando Archivo de Backup anterior'
-    /bin/rm -rf ${BACKUPDIR}/${BZ2FILE}.tar.bz2
+function rename_file {
+    /usr/bin/logger -i -t $(basename $0) 'Renombrando Archivo de Backup Anterior'
+    DATE=$(date --date="1 day ago" "+%F")
+    /bin/mv ${BACKUPDIR}/${BZ2FILE}.tar.bz2 ${BACKUPDIR}/${BZ2FILE}-${DATE}.tar.bz2
 }
 
-function exists {
-if [ -d "$1" ]; then
-    $2
-else
-    /bin/mkdir $1
-    $2
-fi
-}
 function backup {
+    /usr/bin/logger -i -t $(basename $0) 'Deteniendo Postgresql'
+    ${STARTUPSCRIPT} stop
     /usr/bin/logger -i -t $(basename $0) 'Creando SNAPSHOT LVM'
     ### CREATE LVM SNAPSHOT ###
-    /usr/sbin/lvm lvcreate -s -n ${NLVMS} -l 100%FREE ${LVM}
+    /usr/sbin/lvm lvcreate -s -n ${NEW_LVM} -l 100%FREE ${LVM}
     if [ $? -eq 0 ]
     then
         ### STARTING POSTGRESQL ###
@@ -39,42 +28,40 @@ function backup {
         ### MOUNT SNAPSHOT ###
         /usr/bin/logger -i -t $(basename $0) 'Montando Snapshot'
         /bin/mount ${LVMS} ${MNTP}
-        ### ENTER DIRECTORY ###
-        cd /mnt/
-        deletefile
-        ### CREATING BACKUP ###
-        /usr/bin/logger -i -t $(basename $0) 'Creando Backup Postgresql'
-        /bin/tar -cjvf ${BACKUPDIR}/${BZ2FILE}.tar.bz2 pgsql
+        if [ $? -eq 0 ]
+        then
+            rename_file
+            ### CREATING BACKUP ###
+            /usr/bin/logger -i -t $(basename $0) 'Creando Backup Postgresql'
+            cd /mnt/
+            /bin/tar -cjvf ${BACKUPDIR}/${BZ2FILE}.tar.bz2 pgsql
+            if [ $? -eq 0 ]
+            then
+                ### UMOUNT SNAPSHOT ###
+                /usr/bin/logger -i -t $(basename $0) 'Desmontando Snapshot'
+                cd /tmp
+                /bin/umount ${MNTP}
+                ### REMOVE LVM SNAPSHOT ###
+                /usr/bin/logger -i -t $(basename $0) 'Eliminando SNAPSHOT LVM'
+                /usr/sbin/lvm lvremove -f ${LVMS}
+            else
+                /usr/bin/logger -i -t $(basename $0) 'Error al crear archivo bz2'
+            fi
+        else
+            /usr/bin/logger -i -t $(basename $0) 'Error al montar Snapshot'
+        fi
     else
-        /usr/bin/logger -i -t $(basename $0) 'Creando Backup Postgresql'
-    fi
-    ### UMOUNT SNAPSHOT ###
-    /usr/bin/logger -i -t $(basename $0) 'Desmontando Snapshot'
-    /bin/umount ${MNTP}
-    ### REMOVE LVM SNAPSHOT ###
-    /usr/bin/logger -i -t $(basename $0) 'Eliminando SNAPSHOT LVM'
-    /usr/sbin/lvm lvremove -f ${LVMS}
-    exists ${MNTSMB} movebk
-}
-
-function movebk {
-    /usr/bin/logger -i -t $(basename $0) 'Montando Storage Remoto'
-    /bin/mount -t cifs -o domain=${DOMAINSMB},user=${USRSMB},password=${PASSSMB} //${REMOTESMB} ${MNTSMB}
-    if [ $? -eq 0]
-    then
-        /usr/bin/logger -i -t $(basename $0) 'Copiando archivo al Storage Remoto'
-        /bin/cp ${BACKUPDIR}/${BZ2FILE}.tar.bz2 ${MNTSMB}${BZ2FILE}-$(date +%F).tar.bz2
-        ### UMOUNT STORAGE ###
-        /usr/bin/logger -i -t $(basename $0) 'Desmontando Storage Remoto'
-        /bin/umount ${MNTSMB}
-    else
-        /usr/bin/logger -i -t $(basename $0) 'No se pudo montar el Storage Remoto'
+        /usr/bin/logger -i -t $(basename $0) 'Error al crear Snapshot LVM'
     fi
 }
 
+### MAIN PROGRAM ###
 /usr/bin/logger -i -t $(basename $0) 'Iniciando Proceso de backup Postgresql'
-/usr/bin/logger -i -t $(basename $0) 'Deteniendo Postgresql'
-### STOPPING DATABASE ###
-${STARTUPSCRIPT} stop
-
-exists ${MNTP} backup
+cd ${BACKUPDIR}
+if [ $? -eq 0 ]
+then
+    backup
+else
+    /usr/bin/logger -i -t $(basename $0) "directory ${BACKUPDIR} doesn't exists"
+    exit 1
+fi
