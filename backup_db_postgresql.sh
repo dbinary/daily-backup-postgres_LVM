9a4331d355fb4334ac0f948156ff7e524e6023b7
Luis Perez <luis.luimarin@gmail.com>
2016-09-02 10:20:55 -0600

    set header license

--- backup_db_postgresql.sh
+++ backup_db_postgresql.sh
@@ -1,4 +1,26 @@
-#!/bin/bash -x
+#!/bin/bash
+
+# MIT License
+#
+# Copyright (c) 2016 Luis Perez
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
 STARTUPSCRIPT="/etc/init.d/postgresql" # startup script for init postgresql
 LVM="" # path of LV to backup i.e. /dev/VolGroup01/postgresql
